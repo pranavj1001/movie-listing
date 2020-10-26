8e7cbdc456a114f0265c7bc633653dfed60166cb
Pranav Jain <pranavj1001@gmail.com>
2020-10-26 20:51:48 +0530

    update oauth  credentials

--- client/movie-box/src/components/google-auth/google-auth.js
+++ client/movie-box/src/components/google-auth/google-auth.js
@@ -10,7 +10,7 @@ class GoogleAuth extends Component {
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
             window.gapi.client.init({
-                clientId: '885575434437-t5b336u3rjm000i5kovrgim5ckfqias9.apps.googleusercontent.com',
+                clientId: '885575434437-3pgjnumdujqlncrf5fdpu5d8ihg6esn5.apps.googleusercontent.com',
                 scope: 'email'
             }).then(() => {
                 this.auth = window.gapi.auth2.getAuthInstance();
